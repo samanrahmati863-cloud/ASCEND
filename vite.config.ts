7094f5019d2459ed471dc977fa796280f76fb845
samanrahmati863-cloud <samanrahmati863@gmail.com>
2025-12-02 03:56:23 -0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite';
+import path from 'path';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/ASCEND/',
-});
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, '.', '');
+  return {
+    base: '/ASCEND/',
+    
+    server: {
+      port: 3000
