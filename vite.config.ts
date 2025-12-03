54a4ced6e1a371a0bd6a765526eadfad331db7c6
samanrahmati863-cloud <samanrahmati863@gmail.com>
2025-12-03 05:02:02 -0800

    Update vite.config.ts
    Signed-off-by: samanrahmati863-cloud <samanrahmati863@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
+// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ASCEND/',
+  // این تنظیم مسیرها را نسبت به فایل index.html می‌سازد که معمولاً مشکل گیت‌هاب پیج را حل می‌کند
+  base: './', 
 });
