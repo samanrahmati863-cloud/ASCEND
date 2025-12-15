3346543af887121be61f2c2d857fec25e6fc48d5
samanrahmati863-cloud <samanrahmati863@gmail.com>
2025-12-15 12:04:50 -0800

    Update types.ts
    Signed-off-by: samanrahmati863-cloud <samanrahmati863@gmail.com>

--- types.ts
+++ types.ts
@@ -3,15 +3,6 @@
  * SPDX-License-Identifier: Apache-2.0
 */
 
-export interface Project {
-  id: string;
-  title: string;
-  category: string;
-  video: string; // تغییر از image به video
-  description: string;
-  galleryImages: string[];
-}
-
 export interface ServicePackage {
   title: string;
   price: string;
@@ -28,17 +19,8 @@ export interface ChatMessage {
   isError?: boolean;
 }
 
-export interface Artist {
-  id: string;
-  name: string;
-  genre: string;
-  day: string;
-  image: string;
-}
-
 export enum Section {
   HERO = 'hero',
-  WORK = 'work',\
   SERVICES = 'services',
   CONTACT = 'contact',
 }
